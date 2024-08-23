98b1d318cf07e782da6e28e45f0fea996d997f5b
Gonza Rico <gonzaricoesteban@gmail.com>
2024-08-23 19:29:29 +0200

    Player 2.0

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,13 +5,13 @@ const SPEED = 60
 var direction = 1
 
 @onready var ray_cast_right = $RayCastRight
-@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var animated_sprite = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if ray_cast_right.is_colliding():
 		ray_cast_right.target_position = -ray_cast_right.target_position
 		direction = -direction
-		animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h
+		animated_sprite.flip_h = !animated_sprite.flip_h
 		
 	position.x += direction * SPEED * delta
